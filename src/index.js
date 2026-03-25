c0960a58908633cac02f0ea8307990197b21c73b
LondonCityRoleplay <lcrol3play@gmail.com>
2026-03-25 21:21:17 +0000

    src/index.js
    Temporarily disable command and event handlers while logging in.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-require("./handlers/commandHandler")(client);
-require("./handlers/eventHandler")(client);
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+// Disable handlers temporarily
+// require("./handlers/commandHandler")(client);
+// require("./handlers/eventHandler")(client);
 
 client.login(process.env.DISCORD_TOKEN);

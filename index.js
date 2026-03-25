308c720082338d9d655ff22379688c611d40e242
LondonCityRoleplay <lcrol3play@gmail.com>
2026-03-25 19:45:22 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const config = require("./config");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(config.token);
